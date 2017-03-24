f1ff5363014dc977a1998cc7bdf7ea6be5cee2c6
chvoser <christian.voser@students.fhnw.ch>
2017-03-24 19:27:58 +0100

    last version from third schoolday

--- Calculator_A-SWE/src/ch/fhnw/mas/wuvo/MathUtil.java
+++ Calculator_A-SWE/src/ch/fhnw/mas/wuvo/MathUtil.java
@@ -1,7 +1,19 @@
 package ch.fhnw.mas.wuvo;
 
+/**
+ * 
+ * @author chvoser
+ * This class calculate the faculty of the number which
+ * was entered by the user.
+ */
+
 public class MathUtil {
 
+	/**
+	 * 
+	 * @param n input value for calculation
+	 * @return the faculty result 
+	 */
 	public static long faculty(int n)
 	{
 		long result = 1;
